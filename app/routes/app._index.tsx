8da298e3e9ce9969f14de9b53bd7648ab790a37a
hungpv <phamvanhung0412@gmail.com>
2024-09-24 14:25:55 +0700

    fix: api by minhvv

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -3,8 +3,8 @@ import {
 
 } from "@shopify/polaris";
 import Apphomepage from "./components/apphomepage";
-import TitleOnHub from "../routes/components/UI/titleOnHub";
 import Constants from "./Core/Helpers/constants";
+import TitleOnHub from "./components/UI/titleOnhub";
 export default function Index() {
 
   //
